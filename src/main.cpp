0ec9758ec30630d617b27d650effcbf9cb92a031
Karthik <yourboykarthi@gmail.com>
2025-12-30 19:49:14 +0530

    Setup Miniaudio, Audiofile, - Wave form visualizer with audio plays

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
-#include<iostream>
-#include<cstdlib>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cstdlib>
+#include <audiofile/AudioFile.h>
+#include <miniaudio.h>
+#include <chrono>
+
+using namespace std::chrono;
 
 void resize_frame_buffer_on_window(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
@@ -45,10 +50,20 @@ int main(){
 
     std::cout << "Strad has started..." << std::endl;
 
-    float signal[300] = {0};
-    for(int i = 0; i < 300; i+=3){
-        signal[i] = (i/3 * 0.01f) + (-0.5f);
-        signal[i+1] = 0.25f;
+    // AudioFile test
+    AudioFile<double> audioFile;
+    audioFile.load("audio/memphis-trap.wav");
+    audioFile.printSummary();
+    int numSamples = audioFile.getNumSamplesPerChannel();
+    for(int i = 0; i < numSamples; i++){
+        double sample = audioFile.samples[0][i];
+    }
+
+    float signal[16000 * 3] = {0};
+    for(int i = 0; i < 16000 * 3; i+=3){
+        signal[i] = ((float)i/3)/16000 - 0.5f;
+        // std::cout << signal[i] << std::endl;
+        signal[i+1] = audioFile.samples[0][i];
         signal[i+2] = 0.0f;
     }
 
@@ -111,23 +126,63 @@ int main(){
     glDeleteShader(fragmentShader);
     fflush(0);
     
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    ma_result result;
+    ma_engine engine;
+
+    result = ma_engine_init(NULL, &engine);
+    if(result != MA_SUCCESS){
+        std::cout << "Failed to initialize Miniaudio Engine" << std::endl;
+        return -1;
+    } 
+
+    
+    glClearColor(1.0f, 0.0f, 0.5f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    int offset = 0;
+    float prevTime = glfwGetTime(), currTime;
+    int frameCount = 0;
+    auto cPrev = system_clock::now();
+    auto cNow = system_clock::now();
+    ma_engine_play_sound(&engine, "/run/media/karthik/Shared Volume/1_projects/1_dev/strad_audio_visualizer/audio/memphis-trap.wav", NULL);
     while(!glfwWindowShouldClose(window)){
-        for(int i = 0; i < 300; i+=3){
-            signal[i+1] = generateRandomFloat(0, 1);
-            std::cout << signal[i+1] << std::endl;
+        // for(int i = 0; i < 300; i+=3){
+        //     signal[i+1] = generateRandomFloat(0, 1);
+        //     std::cout << signal[i+1] << std::endl;
+        // }
+        for(int i = 0; i < 16000 * 3; i+=3){
+            signal[i+1] = audioFile.samples[0][i + (int)(16000/50.0f) * offset];
         }
+        offset+=1;
         glBindBuffer(GL_ARRAY_BUFFER, signalVao);
         glBufferData(GL_ARRAY_BUFFER, sizeof(signal), signal, GL_STATIC_DRAW);
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shader);
         glBindVertexArray(signalVao);
-        glDrawArrays(GL_LINE_STRIP, 0, 100);
+        glDrawArrays(GL_LINE_STRIP, 0,16000);
+        
+        currTime = glfwGetTime();
+        float change = currTime - prevTime;
+        if(change >= 1){
+            float fps = frameCount / change;
+            std::cout << "FPS: " << fps << std::endl;
+            frameCount = 0;
+            prevTime = currTime;
+        }
+
+        // cNow = system_clock::now();
+        // std::cout << duration_cast<milliseconds>((cNow - cPrev)).count() << std::endl;
+        while(duration_cast<milliseconds>((system_clock::now() - cPrev)).count() <= (1000/50.0f)){
+            
+        }
+        cPrev = system_clock::now();
         glfwSwapBuffers(window);
+        frameCount++;
         glfwPollEvents();
     }
+
+    ma_engine_uninit(&engine);
 
     glfwTerminate();
     return 0;
 }
+ 
