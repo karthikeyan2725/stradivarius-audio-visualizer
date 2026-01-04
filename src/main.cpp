ce64f90a0ba7751f23eaa538ff9cfb5fc72b35e3
Karthik <yourboykarthi@gmail.com>
2026-01-04 09:23:45 +0530

    Moved code to waveform 2 class

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,8 @@
 #include <fftw3/fftw3.h>
 
 #include <iostream>
-#include <chrono>
 #include <vector>
 #include <math.h>
-using namespace std::chrono;
 
 #define APPNAME "Stradivarius"
 #define ONE_SECOND 1000
@@ -18,7 +16,7 @@ void resize_frame_buffer_on_window(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
 }
 
-class Waveform {
+class Waveform1 {
     std::vector<std::vector<double>> audioSignal;
     float *vertexData;
     unsigned int vertexDataSize;
@@ -40,7 +38,7 @@ class Waveform {
             "color = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
         "}\0";
 public:
-    Waveform(std::vector<std::vector<double>> audioSignal){
+    Waveform1(std::vector<std::vector<double>> audioSignal){
         this->audioSignal = audioSignal;
         numVertices = 16000 * 3;
         vertexDataSize = numVertices * sizeof(float);
@@ -108,9 +106,135 @@ public:
         glDrawArrays(GL_LINE_STRIP, 0, 16000);
     }
 
-    ~Waveform(){
+    ~Waveform1(){
         delete vertexData;
     }
+};
+
+class Waveform2 {
+    std::vector<std::vector<double>> audioSignal;
+    int frameSize;
+    fftw_complex *frame, *frequencyBands; 
+    fftw_plan plan;
+    int startIndex;
+    int size;
+    double *frameData; // TODO: Careful when merge
+    int sampleRate;
+    unsigned int frameDataSize; // TODO: Careful when merge
+    unsigned int numVertices;
+    unsigned int offset = 0;
+    unsigned int vao;
+    unsigned int vbo;
+    unsigned int shaderProgram;
+    const char* vertexShaderSrc = 
+        "#version 330 core\n" 
+        "layout (location = 0) in vec3 point;\n"
+        "out vec3 oPoint;\n"
+        "void main(){\n"
+            "gl_Position = vec4(point.x, point.y, point.z, 1.0);\n"
+            "oPoint = point;\n"
+        "}\0"; // TODO: Load from file 
+    const char* fragmentShaderSrc = 
+        "#version 330 core\n"
+        "out vec4 color;\n"
+        "in vec3 oPoint;\n"
+        "void main(){\n"
+            "color = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+        "}\0";
+    public:
+        Waveform2(std::vector<std::vector<double>> audioSignal, int sampleRate){
+            this->audioSignal = audioSignal;
+            this->sampleRate = sampleRate;
+            frameSize = 1024 * 2;
+            frame = new fftw_complex[frameSize];
+            frequencyBands = new fftw_complex[frameSize];
+            plan = fftw_plan_dft_1d(frameSize, frame, frequencyBands, FFTW_FORWARD,  FFTW_MEASURE);   
+            startIndex = (20.0f * frameSize) / sampleRate ;
+            size = ((20000.0f * frameSize)/sampleRate) - startIndex;
+
+            numVertices = size * 3;
+            frameDataSize = numVertices * sizeof(double);
+            frameData = new double[numVertices]; 
+            for(int i = 0; i < numVertices; i+=3){
+                frameData[i] = ((double)i/3)/size - 0.5f;
+                frameData[i+1] = 0.0f;
+                frameData[i+2] = 0.0f;
+            }
+            
+            glGenVertexArrays(1, &vao);
+            glBindVertexArray(vao);
+            
+            glGenBuffers(1, &vbo);
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(GL_ARRAY_BUFFER, frameDataSize, frameData, GL_STATIC_DRAW);
+
+            glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 3 * sizeof(double), (void*)0);
+            glEnableVertexAttribArray(0);
+
+            int success;
+            char message[1000];
+
+            unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertexShader, 1, &vertexShaderSrc, NULL);
+            glCompileShader(vertexShader);
+            glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+            if(!success){
+                glGetShaderInfoLog(vertexShader, 1000, NULL, message);
+                std::cout << "Failed to compile Vertex Shader: " << message << std::endl;
+            }
+
+            unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragmentShader, 1, &fragmentShaderSrc, NULL);
+            glCompileShader(fragmentShader);
+            glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+            if(!success){
+                glGetShaderInfoLog(fragmentShader, 1000, NULL, message);
+                std::cout << "Failed to compile Fragment Shader: " << message << std::endl;
+            }
+
+            shaderProgram = glCreateProgram();
+            glAttachShader(shaderProgram, vertexShader);
+            glAttachShader(shaderProgram, fragmentShader);
+            glLinkProgram(shaderProgram);
+            glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+            if(!success){
+                glGetProgramInfoLog(shaderProgram, 1000, NULL, message);
+                std::cout << "Failed to link shaders: " << message << std::endl;
+            }
+
+            glDeleteShader(vertexShader);
+            glDeleteShader(fragmentShader);
+        }
+
+        void draw(int currentMillisecond){
+            for(int i = 0; i < frameSize; i++){
+                frame[i][0] = audioSignal[0][(currentMillisecond/1000.0f) * sampleRate + i];
+                frame[i][1] = 0;
+            }
+            fftw_execute(plan);
+            int j = startIndex;
+            for(int i = 0; i < numVertices; i+=3){ // TODO: Handle out of bound
+                double real = frequencyBands[j][0];
+                double complex = frequencyBands[j][1];
+                frameData[i+1] =  sqrt(real * real + complex * complex) / (frameSize/8); 
+                j++;
+            }
+
+            glBindBuffer(GL_ARRAY_BUFFER, vao);
+            glBufferData(GL_ARRAY_BUFFER, frameDataSize, frameData, GL_STATIC_DRAW);
+            glUseProgram(shaderProgram);
+            glBindVertexArray(vao);
+            glDrawArrays(GL_LINE_STRIP, 0, size);
+        }
+
+        ~Waveform2(){
+            delete frame;
+            delete frequencyBands;
+            delete frameData;
+            fftw_destroy_plan(plan);
+            fftw_cleanup();
+
+        }
 };
 
 int main(){
@@ -149,87 +273,8 @@ int main(){
     audioFile.load(songPath);
     audioFile.printSummary();
     int sampleRate = audioFile.getSampleRate();
-    // Waveform waveform(audioFile.samples);
-    
-    int frameSize = 1024 * 2;
-    // int hopSize = ((double)sampleRate / frameSize);
-    fftw_complex frame[frameSize], frequencyBands[frameSize];
-    fftw_plan plan = fftw_plan_dft_1d(frameSize, frame, frequencyBands, FFTW_FORWARD,  FFTW_MEASURE);
-    int frameOffset = 0;   
-    int startIndex = (20.0f * frameSize) / sampleRate ;
-    int size = ((20000.0f * frameSize)/sampleRate) - startIndex;
-    // std::cout << "Hop Size : " << hopSize; 
-
-    std::cout << "Start Index:" << startIndex << std::endl;
-    std::cout << "Size: " << size << std::endl;
-
-    int numVertices = size * 3;
-    int frameDataSize = numVertices * sizeof(double);
-    double frameData[numVertices]; 
-    for(int i = 0; i < numVertices; i+=3){
-        frameData[i] = ((double)i/3)/size - 0.5f;
-        frameData[i+1] = 0.0f;
-        frameData[i+2] = 0.0f;
-    }
-
-    unsigned int vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    
-    unsigned int vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, frameDataSize, frameData, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 3 * sizeof(double), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    int success;
-    char message[1000];
-
-    const char* vertexShaderSrc = 
-        "#version 330 core\n" 
-        "layout (location = 0) in vec3 point;\n"
-        "void main(){\n"
-            "gl_Position = vec4(point.x, point.y, point.z, 1.0);\n"
-        "}\0"; // TODO: Load from file 
-    const char* fragmentShaderSrc = 
-        "#version 330 core\n"
-        "out vec4 color;\n"
-        "void main(){\n"
-            "color = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
-        "}\0";
-
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSrc, NULL);
-    glCompileShader(vertexShader);
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if(!success){
-        glGetShaderInfoLog(vertexShader, 1000, NULL, message);
-        std::cout << "Failed to compile Vertex Shader: " << message << std::endl;
-    }
-
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSrc, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if(!success){
-        glGetShaderInfoLog(fragmentShader, 1000, NULL, message);
-        std::cout << "Failed to compile Fragment Shader: " << message << std::endl;
-    }
-
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success){
-        glGetProgramInfoLog(shaderProgram, 1000, NULL, message);
-        std::cout << "Failed to link shaders: " << message << std::endl;
-    }
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    // Waveform1 waveform1(audioFile.samples);
+    Waveform2 Waveform2(audioFile.samples, sampleRate);
 
     ma_result result;
     ma_engine engine;
@@ -246,74 +291,20 @@ int main(){
         glfwTerminate();
         return -1;
     } 
-    // ma_engine_play_sound(&engine, "./audio/memphis-trap.wav", NULL);
     ma_sound_start(&sound);
-    
-    float prevTime = glfwGetTime(), currTime, fps, deltaTime;
-    int frameCount = 0;
-    auto cPrev = system_clock::now();
 
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(0.0f, 0.1f, 0.0f, 1.0f);
     while(!glfwWindowShouldClose(window)){
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // waveform.draw();
-        // ma_int64 f = ma_engine_get_time_in_pcm_frames(&engine);
-        // ma_int64 currentFrame = (double)ma_sound_get_time_in_pcm_frames(&sound);
-        ma_int64 currentSecond = ma_engine_get_time_in_milliseconds(&engine);
-        // std::cout << "Actual: " << f << " Frame: " << currentFrame << " Frame Offset: " << frameOffset << std::endl;
-        // std::cout << currentSecond << std::endl;
-        for(int i = 0; i < frameSize; i++){
-            frame[i][0] = audioFile.samples[0][(currentSecond/1000.0f) * sampleRate + i];
-            frame[i][1] = 0;
-        }
-
-        fftw_execute(plan);
-        // for(int i = 0; i < frameSize; i++){
-        //     std::cout << frequencyBands[i][0] << ", ";
-        // }
-        // std::cout << std::endl;
-
-        int j = startIndex;
-        for(int i = 0; i < numVertices; i+=3){
-            double real = frequencyBands[j][0];
-            double complex = frequencyBands[j][1];
-            frameData[i+1] =  sqrt(real * real + complex * complex) / (frameSize/8); // sqrt(real * real + complex * complex) / ; // Magnitude is the intensity of frequency
-            // std::cout << i << " " << frameData[i] << " " << frameData[i+1] << std::endl;
-            j++;
-        }
-
-        glBindBuffer(GL_ARRAY_BUFFER, vao);
-        glBufferData(GL_ARRAY_BUFFER, frameDataSize, frameData, GL_STATIC_DRAW);
-        glUseProgram(shaderProgram);
-        glBindVertexArray(vao);
-        glDrawArrays( GL_LINE_STRIP, 0, size);
-
-        // std::cout <<    "Frame : " << frameOffset << std::endl;
-        // frameOffset++;
-
-        // currTime = glfwGetTime();
-        // deltaTime = currTime - prevTime;
-        // if(currTime - prevTime > 1){
-        //     fps = frameCount / deltaTime;
-        //     frameCount = 0;
-        //     prevTime = currTime;
-
-        //     glfwSetWindowTitle(window, (APPNAME + std::to_string(fps)).c_str());
-        // }
-
-        // while(duration_cast<milliseconds>((system_clock::now() - cPrev)).count() <= ((float)ONE_SECOND / FPS)){
-            
-        // }
-       
+        
+        ma_int64 currentMillisecond = ma_engine_get_time_in_milliseconds(&engine);
+        // waveform1.draw();
+        Waveform2.draw(currentMillisecond); 
+        
         glfwSwapBuffers(window);
-        cPrev = system_clock::now();
-        frameCount++;
         glfwPollEvents();
     }
 
-    fftw_destroy_plan(plan);
-    fftw_cleanup();
     ma_engine_uninit(&engine);
     ma_sound_uninit(&sound);
     glfwTerminate();
